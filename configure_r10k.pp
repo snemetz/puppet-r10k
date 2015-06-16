9bab0c4f78a9a2e1292d4897586ba7572b9d9493
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-16 11:36:52 -0700

    Update control repo and remove version

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -7,10 +7,11 @@
 ##  Beware! (and good luck!)
 
 class { 'r10k':
-  version           => '1.3.2',
+  #version           => '1.3.2',
   sources           => {
     'puppet' => {
-      'remote'  => 'https://github.com/glarizza/puppet_repository.git',
+      #'remote'  => 'https://github.com/glarizza/puppet_repository.git',
+      'remote'  => 'https://github.com/snemetz/puppet-r10k.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
     }
