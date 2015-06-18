8acab60016621e94b6973a0522c05bee8a25557e
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-18 16:01:59 -0700

    Add script for creating new puppet environments

---
+++ add-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Add a new puppet environment
+
+# Author: Steven Nemetz
+
+# Verify at top of repo
+# .git exists
+puppet_env=$1
+puppet_env_src='Production'
+git checkout $puppet_env_src
+git branch $puppet_env
+git checkout $puppet_env
+sed -i "s/$puppet_env_src/$puppet_env/g" README.txt
+sed -i "s/$puppet_env_src/$puppet_env/g" manifests/README.txt
+sed -i "s/$puppet_env_src/$puppet_env/g" hieradata/README.txt
+git rm *.pp hiera.yaml
+git add --all
+git commit -m "Initial creation of new puppet environment: $puppet_env"
+# Look for git command to do this
+# Needed??
+#cat >> .git/config <<NewBranch
+#[branch "$puppet_env"]
+#	remote = origin
+#	merge = refs/heads/$puppet_env
+#NewBranch
+
+git push --all
+
