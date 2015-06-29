4474263c1dedb65fc4a3b583137c6723115ad98c
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-29 12:05:13 -0700

    Deploy images on Jenkins server

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,9 @@
 node pe-381-agent-jenkins {
   Class['Epel'] -> Class['Jenkins_job_builder::Config']
+  Class['Jenkins'] -> Class['Files']
   include epel
   include jenkins
   include jenkins_job_builder
+  include files
+  files::list{'jenkins-master':}
 }
